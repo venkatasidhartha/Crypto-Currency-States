205ea742066a7d54a93c377efd6a657363aaba38
venkatasidhartha <38367008+venkatasidhartha@users.noreply.github.com>
2020-11-06 10:05:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,38 @@
 import requests
 from prettytable import PrettyTable
 
-def request(URL):
-    request = requests.get(URL).json()
-    data = request["data"]
-    dataStorage(data)
+class cryptoCurrency:
+    def request(self,URL):
+        request = requests.get(URL).json()
+        # getting data in form of dictionary
+        data = request["data"]
+        self.dataStorage(data)
 
-def dataStorage(data):
-    id = []
-    rank = []
-    priceUsd = []
-    changePercent24Hr = []
-    for dataList in data:
-        #converting list to dictionary
-        dict = dataList
-        id.append(dict["id"])
-        rank.append(dict["rank"])
-        priceUsd.append(dict["priceUsd"])
-        changePercent24Hr.append(dict["changePercent24Hr"])
-    displayTables(rank,id,priceUsd,changePercent24Hr)
+    def dataStorage(self,data):
+        id = []
+        rank = []
+        priceUsd = []
+        changePercent24Hr = []
+        # converting dictionary to list
 
-def displayTables(rank,id,priceUsd,changePercent24Hr):
-    Columns = ["Rank", "Name", "Price", "Percentage"]
-    myTable = PrettyTable()
-    myTable.add_column(Columns[0], rank)
-    myTable.add_column(Columns[1], id)
-    myTable.add_column(Columns[2], priceUsd)
-    myTable.add_column(Columns[3], changePercent24Hr)
-    print(myTable)
+        for dataList in data:
+            # converting list to dictionary
+            dict = dataList
+            id.append(dict["id"])
+            rank.append(dict["rank"])
+            priceUsd.append(dict["priceUsd"])
+            changePercent24Hr.append(dict["changePercent24Hr"])
+        self.displayTables(rank, id, priceUsd, changePercent24Hr)
 
+    def displayTables(self,rank, id, priceUsd, changePercent24Hr):
+        Columns = ["Rank", "Name", "Price", "Percentage"]
+        myTable = PrettyTable()
+        myTable.add_column(Columns[0], rank)
+        myTable.add_column(Columns[1], id)
+        myTable.add_column(Columns[2], priceUsd)
+        myTable.add_column(Columns[3], changePercent24Hr)
+        print(myTable)
 
+currency = cryptoCurrency()
 ApiUrl = "https://api.coincap.io/v2/assets"
-request(ApiUrl)
+currency.request(ApiUrl)
